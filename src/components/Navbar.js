e61f75d32265f4435c3deb5d51ac43287f25b74a
Shri Ganesh Jha <realsgj@gmail.com>
2022-08-20 17:08:40 +0000

    master branch stable version

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -113,7 +113,7 @@ const Navbar = () =>{
                 </Link>
               </li>
               {/* popular categories */}
-              {/* <li>
+              <li>
                 <Link to="/business"
                   onClick={clickNavClose}
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
@@ -160,7 +160,7 @@ const Navbar = () =>{
                 >
                   Entertainment
                 </Link>
-              </li> */}
+              </li>
               {/* end popular categories */}
               <li>
                 <Link to="/services"
